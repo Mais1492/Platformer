642eae098bf4f9c5f7d93e3c8e3d19dece8f90e8
Mais <mayrmichael@gmx.net>
2026-05-05 08:31:02 +0200

    feat: add level up logic

--- src/UI/ui.gd
+++ src/UI/ui.gd
@@ -1,10 +1,11 @@
 extends Control
 
+var level := 1
 var score = 0	
 var player = null
 var buff_active := false
-
-@onready var label: Label = $Label
+@onready var score_label: Label = $"Score-Label"
+@onready var level_label: Label = $"Level-Label"
 @onready var border: Panel = $Border
 
 func set_player(p):
@@ -25,4 +26,14 @@ func _process(delta):
 
 func add_point():
 	score += 1
-	label.text = "Score: " + str(score)
+	score_label.text = "Score: " + str(score)
+	
+	var new_level = int(score / 3) + 1
+	
+	if (new_level > level):
+		level = new_level
+		_on_level_up()
+		
+func _on_level_up():
+	level_label.text = "Level: " + str(level)
+	print("LEVEL UP!")
